24acdb56291dbfc84d59616f0b396d00497fd801
luke dalgleish <lukedalgleish07@gmail.com>
2024-09-10 23:00:35 +0100

    Highscore UI
    Set up a very ugly looking Highscore UI page. The important thing is it works! Still requires testing for ensure its bug free though.

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,7 +7,7 @@ public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private GameObject highScoreUI, optionsUI;
     private bool highScoreUIBool, optionsUIBool = false;
-    
+    [SerializeField] TextMeshProUGUI HighscoreRankings;
 
     // Start is called before the first frame update
     public void StartGame()
@@ -25,12 +26,21 @@ public class MainMenuUI : MonoBehaviour
         {
             OptionsUIDisabled();
         }
+
+        HighscoreRankings.text = 
+            HighscoreLogic.Instance.highscoreNames[0] + " --- " +  " Rounds Survived: " + HighscoreLogic.Instance.highscores[0] + "\n" +
+            HighscoreLogic.Instance.highscoreNames[1] + " --- " + " Rounds Survived: " + HighscoreLogic.Instance.highscores[1] + "\n" +
+            HighscoreLogic.Instance.highscoreNames[2] + " --- " + " Rounds Survived: " + HighscoreLogic.Instance.highscores[2] + "\n" +
+            HighscoreLogic.Instance.highscoreNames[3] + " --- " + " Rounds Survived: " + HighscoreLogic.Instance.highscores[3] + "\n" +
+            HighscoreLogic.Instance.highscoreNames[4] + " --- " + " Rounds Survived: " + HighscoreLogic.Instance.highscores[4];
+
     }
 
     public void HighScoreUIEnabled()
     {
         highScoreUI.SetActive(true);
         highScoreUIBool = true;
+
     }
     public void HighScoreUIDisabled()
     {
@@ -48,6 +58,9 @@ public class MainMenuUI : MonoBehaviour
         optionsUI.SetActive(false);
         optionsUIBool = false;
     }
+
+
+
 
     public void ExitGame()
     {
